779a3bd4cd003ca5b7df4a271843a35755bba484
Josh McArthur <joshua.mcarthur@gmail.com>
2025-04-10 11:33:24 +1200

    Deno: Format fixtures

--- core/test/fixtures/factory_plugin.ts
+++ core/test/fixtures/factory_plugin.ts
@@ -1,5 +1,1 @@
-export default () => ({
-  name: "factory-plugin",
-  version: "1.1",
-  capabilities: [],
-});
+export default () => ({ name: "factory-plugin", version: "1.1", capabilities: [] });
--- core/test/fixtures/valid.config.ts
+++ core/test/fixtures/valid.config.ts
@@ -1,11 +1,13 @@
-export default {
-  storage: { events: { plugin: "memory-storage", options: {} } },
-  plugins: {
-    sources: [
-      "./relative_plugin.ts", // Relative path
-      "https://example.com/remote_plugin.ts", // Absolute URL
-      "/absolute_plugin.ts", // Absolute path
-    ],
-    config: {/* plugin specific config */},
-  },
-};
+
+    export default {
+      storage: { events: { plugin: "memory-storage", options: {} } },
+      plugins: {
+        sources: [
+          "./relative_plugin.ts", // Relative path
+          "https://example.com/remote_plugin.ts", // Absolute URL
+          "/absolute_plugin.ts" // Absolute path
+        ],
+        config: { /* plugin specific config */ }
+      }
+    };
+  
